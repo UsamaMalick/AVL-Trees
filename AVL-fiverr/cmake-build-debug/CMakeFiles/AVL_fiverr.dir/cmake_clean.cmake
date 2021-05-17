9cee924510ff9113c04d0804265d5fcae2e2817a
UsamaMalcik <usamamalick22@gmail.com>
2021-05-17 23:31:52 +0500

    initial commit

---
+++ AVL-fiverr/cmake-build-debug/CMakeFiles/AVL_fiverr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AVL_fiverr"
+  "AVL_fiverr.pdb"
+  "CMakeFiles/AVL_fiverr.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVL_fiverr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
